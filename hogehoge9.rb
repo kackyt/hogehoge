642266c092cb5cd28160f81a5087846f1bfd56b0
kackyt <t_kaki@nextappli.com>
2018-12-28 17:05:52 +0900

    hohoho

---
+++ hogehoge9.rb
@@ -0,0 +1,26 @@
+
+$id = 0
+
+puts "hello,world"
+
+
+if $id < 0
+  puts "hogehoge"
+end
+
+class Hoge
+  
+  def initialize
+    aaa = 1
+    
+    if aaa < 0
+      aaa = 2
+    end
+    
+    
+  end
+  
+  
+end
+
+$xx = 22
